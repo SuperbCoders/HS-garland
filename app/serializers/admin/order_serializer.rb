53bf83cf61b6bca4961ff80ed3e5ad37bc25dab5
STeiN516 <stein516@gmail.com>
2016-08-23 10:04:37 +0500

    В админке отображаются доп опции по доставке и остальное. 1час

--- app/serializers/admin/order_serializer.rb
+++ app/serializers/admin/order_serializer.rb
@@ -1,7 +1,7 @@
 class Admin::OrderSerializer < Admin::BaseSerializer
   attributes :total_price, :status, :rent, :customer, :order_garlands,
       :need_installation, :rain_protection, :start_date, :end_date, :days,
-      :c_at, :order_id, :delivery_address, :guaranty_amount
+      :c_at, :order_id, :delivery_address, :guaranty_amount, :delivery
 
   has_many :order_garlands, serializer: Admin::OrderGarlandSerializer
 
